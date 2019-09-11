1840732d5f43dbcb74cecee226d1a3f8e3a77b44
puwasala-sj <projectmad07@gmail.com>
2019-09-11 07:34:19 +0530

    Commit two

--- app/src/main/java/com/example/nomnom/MainActivity.java
+++ app/src/main/java/com/example/nomnom/MainActivity.java
@@ -2,14 +2,42 @@ package com.example.nomnom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button login;
+    private Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openLoginActivity();
+            }
+        });
+        register = (Button) findViewById(R.id.register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openRegisterActivity();
+            }
+        });
+    }
+    public void openLoginActivity(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void openRegisterActivity(){
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
     }
 }
