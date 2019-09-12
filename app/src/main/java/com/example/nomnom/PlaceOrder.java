b11e2afbe11606a359bb09d8071cebf5738c4992
Maheshi97 <mslewkebandara@gmail.com>
2019-09-12 13:09:05 +0530

    commitEight

--- app/src/main/java/com/example/nomnom/PlaceOrder.java
+++ app/src/main/java/com/example/nomnom/PlaceOrder.java
@@ -3,12 +3,24 @@ package com.example.nomnom;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class PlaceOrder extends AppCompatActivity {
+   /* DatabaseHelper myDb;
+    EditText editName, editAddress, editQuantity;
+    Button btnAddData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place_order);
-    }
+        myDb = new DatabaseHelper(this);
+
+        editName = (EditText)findViewById(R.id.editText_name);
+        editAddress = (EditText)findViewById(R.id.editText_address);
+        editQuantity = (EditText)findViewById(R.id.editText_quantity);
+        btnAddData = (Button)findViewById(R.id.order);
+        AddData();   */
+
 }
