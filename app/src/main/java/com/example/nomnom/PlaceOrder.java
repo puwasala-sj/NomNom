084b9ac107727c63054ccb26c52740c2e3a8e9c7
Maheshi97 <mslewkebandara@gmail.com>
2019-09-13 08:17:06 +0530

    commitTwenty

--- app/src/main/java/com/example/nomnom/PlaceOrder.java
+++ app/src/main/java/com/example/nomnom/PlaceOrder.java
@@ -3,11 +3,13 @@ package com.example.nomnom;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class PlaceOrder extends AppCompatActivity {
-   /* DatabaseHelper myDb;
+    DatabaseHelper myDb;
     EditText editName, editAddress, editQuantity;
     Button btnAddData;
 
@@ -17,10 +19,29 @@ public class PlaceOrder extends AppCompatActivity {
         setContentView(R.layout.activity_place_order);
         myDb = new DatabaseHelper(this);
 
-        editName = (EditText)findViewById(R.id.editText_name);
-        editAddress = (EditText)findViewById(R.id.editText_address);
-        editQuantity = (EditText)findViewById(R.id.editText_quantity);
-        btnAddData = (Button)findViewById(R.id.order);
-        AddData();   */
+        editName = (EditText) findViewById(R.id.editText_name);
+        editAddress = (EditText) findViewById(R.id.editText_address);
+        editQuantity = (EditText) findViewById(R.id.editText_quantity);
+        btnAddData = (Button) findViewById(R.id.order);
+        AddData();
 
+    }
+
+    public void AddData() {
+        btnAddData.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        boolean isInserted = myDb.insertData(editName.getText().toString(),
+                                editAddress.getText().toString(),
+                                editQuantity.getText().toString());
+                        if (isInserted = true)
+                            Toast.makeText(PlaceOrder.this, "Data Inserted Successfully", Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(PlaceOrder.this, "Data Not Inserted", Toast.LENGTH_LONG).show();
+
+                    }
+                }
+        );
+    }
 }
