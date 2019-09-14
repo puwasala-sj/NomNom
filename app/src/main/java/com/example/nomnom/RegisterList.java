4d536c9a566292d2409624e98ce3faa6cc1bb56e
puwasala-sj <projectmad07@gmail.com>
2019-09-14 19:52:25 +0530

    commit61

--- app/src/main/java/com/example/nomnom/RegisterList.java
+++ app/src/main/java/com/example/nomnom/RegisterList.java
@@ -3,12 +3,39 @@ package com.example.nomnom;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.nomnom.Database.DatabaseHelper;
 
 public class RegisterList extends AppCompatActivity {
+    DatabaseHelper db;
+    EditText username;
+    EditText email;
+    EditText password;
+
+    Button list;
+    Button delete;
+    Button update;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_list);
+
+        list = (Button) findViewById(R.id.listUser);
+        delete = (Button)findViewById(R.id.deleteUser);
+        update = (Button)findViewById(R.id.updateUser);
+        viewUsers();
+    }
+
+    private void viewUsers() {
+        list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
