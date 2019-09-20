9fd3c3897edfe135107bfdb422a5d71b8409f8ef
puwasala-sj <projectmad07@gmail.com>
2019-09-20 20:22:37 +0530

    commit110

--- app/src/main/java/com/example/nomnom/AboutUs.java
+++ app/src/main/java/com/example/nomnom/AboutUs.java
@@ -2,13 +2,29 @@ package com.example.nomnom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AboutUs extends AppCompatActivity {
+
+    Button feedback;
+    Button contact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+        feedback = (Button) findViewById(R.id.feedback);
+        feedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AboutUs.this, FeedBack.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/nomnom/Home.java
+++ app/src/main/java/com/example/nomnom/Home.java
@@ -4,33 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
 public class Home extends AppCompatActivity {
     Button menu;
-    Button feedback;
     Button hall;
+    Button Aboutus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        Aboutus = (Button) findViewById(R.id.aboutnom2);
+        Aboutus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this, AboutUs.class);
+                startActivity(intent);
+            }
+        });
+
         menu = (Button) findViewById(R.id.menu);
         menu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Home.this, PlaceOrder.class);
-                startActivity(intent);
-            }
-        });
-        feedback = (Button) findViewById(R.id.feedback);
-        feedback.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Home.this, FeedBack.class);
                 startActivity(intent);
             }
         });
